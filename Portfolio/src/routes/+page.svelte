5a293d297af84393d6fddef074bb00d1c2fa5931
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2023-11-16 09:25:02 +0100

    add 2020-2021

---
+++ Portfolio/src/routes/+page.svelte
@@ -0,0 +1,200 @@
+<script>
+    import logo from "./immagini/logo.png";
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".topnav").style.top = "0";
+        } else {
+            document.querySelector(".topnav").style.top = "-70px";
+        }
+        prevScrollpos = currentScrollPos;
+    };
+</script>
+
+<main>
+    <header>
+        <h1>Il mio portfolio di educazione civica</h1>
+    </header>
+
+    <section>
+        <div class="topnav">
+            <img class="logo" src={logo} alt="logo.png" />
+            <a class="active" href="+page.svelte"><b>Home</b></a>
+            <a href="#"><b>2023</b></a>
+            <a href="#"><b>2022</b></a>
+            <a href="#"><b>2021</b></a>
+            <a href="/2020-2021"><b>2020</b></a>
+        </div>
+    </section>
+
+    <div class="container">
+        <div class="right">
+            <h2>
+                Ciao, sono Alessandro Scattaglia della 5° informatica. Sono nato
+                il 19 ottobre 2005 a Savigliano (CN) e frequento l'<a
+                    href="https://agnelli.it/"
+                    target="_blank">Istituto Internazionale Edoardo Agnelli</a
+                >. Questo è il mio portfolio su tutti gli argomenti che abbiamo
+                trattato durante i 5 anni qui.
+            </h2>
+            <br />
+            <h2>
+                Qui sotto è possibile vedere tutto il codice per fare questo
+                portfolio.
+            </h2>
+
+            <div class="center">
+                <center>
+                    <a
+                        href="https://github.com/Alessandro-Scattaglia/Portfolio_svelte"
+                        target="_blank"
+                    >
+                        <img
+                            class="img0"
+                            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                            alt="GitHub Logo"
+                        />
+                    </a>
+                </center>
+            </div>
+        </div>
+        <div class="center" />
+        <div class="left">
+            <div class="video">
+                <iframe
+                    class="video1"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/ulUgpQHHkRo"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                />
+            </div>
+            <br /><br />
+            <h2>
+                Spero che questo sito possa essere un'utile risorsa per voi che
+                lo state guardando.
+            </h2>
+        </div>
+    </div>
+    <br /><br />
+    <footer>
+        <h4>Copyright &copy; Alessandro Scattaglia</h4>
+    </footer>
+</main>
+
+<style>
+    :root {
+        background-color: #207cca;
+    }
+
+    main {
+        font-family: "Arial", sans-serif;
+        max-width: 1000px;
+        padding: 15px;
+        background-color: #fff;
+        border-radius: 40px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        margin: 60px auto;
+    }
+
+    header {
+        text-align: center;
+        font-size: 24px;
+        color: #333;
+        margin-bottom: 20px;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 70px;
+        text-shadow: 2px 2px 4px black;
+        font-weight: bold;
+    }
+
+    nav ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        text-align: center;
+    }
+
+    nav li {
+        display: inline;
+        margin-right: 10px;
+    }
+
+    nav a {
+        text-decoration: none;
+        color: #3498db;
+        font-weight: bold;
+        font-size: 16px;
+        transition: color 0.3s ease;
+    }
+
+    nav a:hover {
+        color: #207cca;
+    }
+
+    footer {
+        text-align: center;
+        color: #777;
+        margin-top: 20px;
+    }
+    .img0 {
+        width: 140px;
+        margin-left: 600px;
+        margin-bottom: -200px;
+    }
+
+    .topnav {
+        transition: top 0.3s;
+        position: fixed;
+        top: 7px;
+        left: 0;
+        right: 0;
+        width: 40%;
+        background-color: #ffffff;
+        overflow: hidden;
+        border-radius: 8px;
+        z-index: 100;
+        margin: 0 auto;
+        border: 1px solid rgb(10, 226, 10);
+    }
+
+    .topnav a {
+        font-family: Tahoma, Verdana, sans-serif;
+        float: right;
+        color: black;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    .topnav a:hover {
+        background-color: rgb(10, 226, 10);
+        color: black;
+        transition: all 0.2s ease 0s;
+        border-radius: 8px;
+    }
+
+    .topnav a.active {
+        float: left;
+        background-color: rgb(10, 226, 10);
+        color: black;
+        border-radius: 8px;
+    }
+
+    .logo {
+        text-align: left;
+        padding-top: 0px;
+        max-width: 94px;
+    }
+    .video1 {
+        border-radius: 15px;
+    }
+</style>
---
+++ Portfolio/src/routes/2020-2021/+page.svelte
@@ -0,0 +1,263 @@
+<script>
+    import logo from "../immagini/logo.png";
+    import pietro from "../immagini/pietro.jpg";
+    import covid from "../immagini/covid19.png";
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".topnav").style.top = "0";
+        } else {
+            document.querySelector(".topnav").style.top = "-70px";
+        }
+        prevScrollpos = currentScrollPos;
+    };
+</script>
+
+<main>
+    <header>
+        <h1>2020-2021</h1>
+    </header>
+
+    <section>
+        <div class="topnav">
+            <img class="logo" src={logo} alt="logo.png" />
+            <a class="active" href="/"><b>Home</b></a>
+            <a href="#"><b>2023</b></a>
+            <a href="#"><b>2022</b></a>
+            <a href="#"><b>2021</b></a>
+            <a href="/2020-2021"><b>2020</b></a>
+        </div>
+    </section>
+
+    <div class="centro">
+        <h2>LETTERE ITALIANE</h2>
+        <p>
+            Durante questa materia abbiamo affrontato approfonditamente il
+            complesso tema della migrazione di persone provenienti dal Nord
+            Africa verso il Sud Italia, con particolare attenzione a Lampedusa.
+            Inoltre, abbiamo avuto l'opportunità di partecipare a un incontro
+            con il dottor <a
+                href="https://it.wikipedia.org/wiki/Pietro_Bartolo"
+                target="_blank">Pietro Bartolo</a
+            >, una figura di spicco nell'ambito dell'assistenza ai migranti e
+            della promozione dei diritti umani.
+        </p>
+
+        <div class="immg1">
+            <img
+                class="img1"
+                src={pietro}
+                width="1800"
+                height="1014"
+                alt="pietro.jpg"
+            />
+        </div>
+        <p>
+            Inoltre, abbiamo avuto l'opportunità di leggere il libro "Le Stelle
+            di Lampedusa" scritto dal dottor Pietro Bartolo, che ci ha offerto
+            uno sguardo toccante sulle esperienze dei residenti e dei migranti
+            sull'isola. Abbiamo anche avuto l'opportunità di vedere il film
+            "Fuocoammare" di Gianfranco Rosi, un documentario che racconta la
+            vita quotidiana a Lampedusa e il dramma delle migrazioni attraverso
+            testimonianze e immagini potenti. Entrambi ci hanno profondamente
+            sensibilizzato sulla complessità e l'umanità di questa situazione.
+            <br /><br />
+        </p>
+        <div class="container">
+            <div class="sinistra">
+                <iframe
+                    class="video1"
+                    width="440"
+                    height="340"
+                    src="https://www.youtube.com/embed/eHEZwiyect4"
+                    title="L'Agnelli incontra il dottor Pietro Bartolo"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                />
+            </div>
+            <div class="destra">
+                <p>
+                    Ecco a voi l'intervista a Pietro Bartolo il 18 novembre 2020
+                </p>
+            </div>
+        </div>
+        <br />
+        <h2>BIOLOGIA</h2>
+        <p>
+            Di biologia durante l'anno scolastico, abbiamo studiato
+            approfonditamente il COVID-19, una malattia infettiva causata dal
+            coronavirus SARS-CoV-2, che ha avuto un impatto significativo sulla
+            salute globale, mettendo in evidenza l'importanza della ricerca
+            scientifica, della prevenzione delle malattie e della collaborazione
+            internazionale nella gestione delle pandemie.
+        </p>
+        <div class="immg2">
+            <img class="img2" src={covid} alt="covid19.png" />
+        </div>
+        <p>
+            Abbiamo esplorato i modi in cui il virus si diffonde, i sintomi
+            associati e le strategie di prevenzione, compresi i vaccini, che
+            sono stati sviluppati a una velocità straordinaria per combattere
+            questa pandemia globale. La pandemia da COVID-19 è stata un esempio
+            illuminante di come la biologia possa essere una disciplina vitale
+            per comprendere e risolvere le sfide emergenti nella salute pubblica
+            e nella medicina.
+        </p>
+        <h2>INFORMATICA</h2>
+        <p>
+            Quest'anno, nel corso delle lezioni di informatica, abbiamo avuto
+            l'opportunità di creare il nostro portfolio utilizzando Google
+            Sites. Attraverso questa piattaforma, abbiamo avuto modo di
+            raccogliere e organizzare in modo completo tutti gli argomenti
+            affrontati durante gli anni di studio di educazione civica.
+        </p>
+    </div>
+
+    <footer>
+        <h4>Copyright &copy; Alessandro Scattaglia</h4>
+    </footer>
+</main>
+
+<style>
+    :root {
+        background-color: #207cca;
+    }
+
+    main {
+        font-family: "Arial", sans-serif;
+        max-width: 1000px;
+        margin: 60px auto;
+        padding: 15px;
+        background-color: #fff;
+        border-radius: 40px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    header {
+        text-align: center;
+        font-size: 24px;
+        color: #333;
+        margin-bottom: 20px;
+    }
+    .video1 {
+        border-radius: 15px;
+    }
+
+    h1 {
+        font-weight: bold;
+    }
+    .topnav {
+        transition: top 0.3s;
+        position: fixed;
+        top: 7px;
+        left: 0;
+        right: 0;
+        width: 40%;
+        background-color: #ffffff;
+        overflow: hidden;
+        border-radius: 8px;
+        z-index: 100;
+        margin: 0 auto;
+    }
+
+    .topnav a {
+        font-family: Tahoma, Verdana, sans-serif;
+        float: right;
+        color: black;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    .topnav a:hover {
+        background-color: rgb(10, 226, 10);
+        color: black;
+        transition: all 0.2s ease 0s;
+        border-radius: 8px;
+    }
+
+    .topnav a.active {
+        float: left;
+        background-color: rgb(10, 226, 10);
+        color: black;
+        border-radius: 8px;
+    }
+
+    .logo {
+        text-align: left;
+        padding-top: 0px;
+        max-width: 94px;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 70px;
+        text-shadow: 2px 2px 4px black;
+    }
+
+    .centro {
+        margin: auto;
+        width: 100%;
+
+        border-radius: 4px;
+        padding: 5px;
+    }
+
+    h2 {
+        padding: 10px;
+        font-size: xx-large;
+        color: rgb(10, 226, 10);
+        text-align: center;
+    }
+
+    p {
+        padding-top: 0;
+        padding: 15px;
+        text-align: justify;
+        font-size: x-large;
+    }
+
+    .immg1 {
+        text-align: center;
+    }
+
+    .img1 {
+        border-radius: 20px;
+        width: 70%;
+        height: 70%;
+    }
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .sinistra {
+        float: none;
+        height: auto;
+        box-sizing: border-box;
+        padding-left: -10px;
+    }
+
+    .destra {
+        float: right;
+        height: auto;
+        box-sizing: border-box;
+    }
+    .immg2 {
+        text-align: center;
+    }
+    .img2 {
+        width: 50%;
+        height: 50%;
+    }
+    footer {
+        text-align: center;
+        color: #777;
+        margin-top: 20px;
+    }
+</style>
