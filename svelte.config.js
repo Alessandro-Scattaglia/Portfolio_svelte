c3cf917d99626f84b5197aadb0025ade2f62e3ec
Alessandro-Scattaglia <alessandro.scattaglia@istitutoagnelli.it>
2024-04-10 10:33:23 +0200

    fix routes

--- svelte.config.js
+++ svelte.config.js
@@ -25,7 +25,7 @@ const config = {
     kit: {
         adapter: adapter(),
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/Portfolio_svelte' : '',
+            base: '/Portfolio_svelte',
         }
     }
 };
